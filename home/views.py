58e7a63a2fced5a4b6c7a6ce8ffe0a54fdac6762
felipealmeidasilva <filipealmeidakukri@hotmail.com>
2023-09-25 14:44:48 -0300

    adicao da estilizacao e inputs de cadastro

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
+
 def home(request):
-    return HttpResponse('Estou em Home')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        return HttpResponse('Fui acionado')
